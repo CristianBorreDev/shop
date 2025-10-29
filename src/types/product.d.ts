4b14fd48619e83b54e9f6645bc8b3cf833459ce4
CristianBorreDev <crstnborre@gmail.com>
2025-10-29 18:24:16 -0500

    feat se ajusto y agrego muchas cualiades en el diseño, la card, carrito, categorias, detalles, tipado, mejora en la experiencia de usuario

--- src/types/product.d.ts
+++ src/types/product.d.ts
@@ -17,7 +17,7 @@ export interface Product {
   featured?: boolean;                  // Si es destacado
   description: string;                 // Descripción larga
   shortDescription?: string;           // Descripción corta
-  specifications?: Record<string, string>; // Ficha técnica
+  specifications?: Record<string, string> | undefined; // Ficha técnica
   variantGroups?: VariantGroup[];      // Grupos de variantes (Color, Talla, etc.)
   variants?: Variant[];                // Combinaciones finales de variantes
   tags?: string[];                     // Palabras clave
